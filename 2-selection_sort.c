b221877b2a9d23d2ac1498e6540502643f8c91ca
Lesego0773 <kabelomakokga@gmail.com>
2024-02-20 15:46:03 -0800

    selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,27 +1,38 @@
 #include "sort.h"
 
 /**
- * selection_sort - It selects the smallest element from the unsorted sublist.
- * and moves it to the end of the sorted sublist.
- * 
- * Return - void.
-*/
+ * selection_sort - Sorts an array of integers using selection sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t d, v;
+    int min_index;
 
-void selection_sort(int *array, size_t size){
+    if (!array || size < 2)
+        return;
 
-  int min_index;
+    for (d = 0; d < size - 1; d++)
+    {
+        min_index = d;
 
-  for (int d = 0; d < size - 1; d++) {
+        for (v = d + 1; v < size; v++)
+        {
+            if (array[v] < array[min_index])
+            {
+                min_index = v;
+            }
+        }
 
-    min_index = d;
-    for(int v = d + 1; v < d; v++){
-        if (array[v] > array[min_index]){
-            min_index = v;
+        /* Explicitly cast min_index to size_t */
+        if ((size_t)min_index != d)
+        {
+            int temp = array[d];
+            array[d] = array[min_index];
+            array[min_index] = temp;
+            print_array(array, size);
         }
     }
-
-    int temp = array[d];
-    array[d] = array[min_index];
-    array[min_index] = temp;
-  } 
 }
+
